3fd6a8369c4978bf669a7cb723c3ae0adfe43d53
maax <a@a.a>
2023-10-30 02:48:19 +0200

    002

--- main.py
+++ main.py
@@ -1,43 +1,16 @@
-# перше завдання
+def is_even(number):
+    return number % 2 == 0
 
-class Animal:
-    habitat = "Unknown"
+user_input = input("Введіть число для перевірки: ")
 
-    def __init__(self, name, legs, can_fly=False, can_swim=False, sound="Unknown"):
-        self.name = name
-        self.legs = legs
-        self.can_fly = can_fly
-        self.can_swim = can_swim
-        self.sound = sound
+try:
+    number_to_check = int(user_input)
+    result = is_even(number_to_check)
 
-    def make_sound(self):
-        print(f"{self.name} says {self.sound}")
+    if result:
+        print(f"{number_to_check} є парним числом.")
+    else:
+        print(f"{number_to_check} є непарним числом.")
 
-    def move(self):
-        if self.can_fly:
-            print(f"{self.name} is flying.")
-        elif self.can_swim:
-            print(f"{self.name} is swimming.")
-        else:
-            print(f"{self.name} is walking.")
-
-tiger = Animal(name="Tiger", legs=4, sound="Roar")
-eagle = Animal(name="Eagle", legs=2, can_fly=True, sound="Screech")
-fish = Animal(name="Fish", legs=0, can_swim=True, sound="Bubble")
-snake = Animal(name="Snake", legs=0, sound="Hiss")
-elephant = Animal(name="Elephant", legs=4, sound="Trumpet")
-
-tiger.make_sound()
-tiger.move()
-
-eagle.make_sound()
-eagle.move()
-
-fish.make_sound()
-fish.move()
-
-snake.make_sound()
-snake.move()
-
-elephant.make_sound()
-elephant.move()
+except ValueError:
+    print("Введено некоректне число. Будь ласка, введіть ціле число.")
